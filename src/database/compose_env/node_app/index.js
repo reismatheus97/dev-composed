cce3666181ed0d5367e01afea97e944038ce18fb
Matheus Reis <matheus.reis@petragold.com.br>
2019-11-21 18:41:37 -0300

    Add h3 on node_app

--- src/database/compose_env/node_app/index.js
+++ src/database/compose_env/node_app/index.js
@@ -6,11 +6,11 @@ const PORT = '3000'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h3>Aplicação executando!</h3>')
+    res.send('<h3>App running!!!</h3>')
 })
 
 app.get('/hello', (req, res) => {
-    res.send('<h3>Hello world my friends!!</h3>')
+    res.send('<h3>Hello world!</h3>')
 })
 
 app.listen(PORT, HOST)
