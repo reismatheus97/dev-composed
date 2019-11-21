4fda6e4f84ebc5185b8f4a663196a884dc909d2e
MatheusReis <matheus.reis@efabrika.com.br>
2019-11-21 02:04:56 -0200

    Add /hello on node_app

--- src/database/compose_env/node_app/index.js
+++ src/database/compose_env/node_app/index.js
@@ -6,7 +6,11 @@ const PORT = '3000'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world my friends!!')
+    res.send('<h3>Aplicação executando!</h3>')
+})
+
+app.get('/hello', (req, res) => {
+    res.send('<h3>Hello world my friends!!</h3>')
 })
 
 app.listen(PORT, HOST)
