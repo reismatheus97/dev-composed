50b828acc89cf9490e76223bc4a84c0439f70b00
MatheusReis <matheus.reis@efabrika.com.br>
2019-11-28 22:07:08 -0200

    Dist file

--- dist_electron/index.js
+++ dist_electron/index.js
@@ -1209,7 +1209,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var elec
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__(/*! /home/reismatheus97/Workspace/Node/electron/dev-composed/src/background.js */\"./src/background.js\");\n\n\n//# sourceURL=webpack:///multi_./src/background.js?");
+eval("module.exports = __webpack_require__(/*! /home/reismatheus97/docker-workspace/dev-composed/src/background.js */\"./src/background.js\");\n\n\n//# sourceURL=webpack:///multi_./src/background.js?");
 
 /***/ }),
 
